437a8f0785b30ac9b95259babb1ec954474e0a49
chimchimster <jorddyishere@gmail.com>
2023-01-25 17:23:11 +0500

    added postgreSQL database to project

--- privatesite/blog/templatetags/blog_tags.py
+++ privatesite/blog/templatetags/blog_tags.py
@@ -5,9 +5,8 @@ register = template.Library()
 @register.inclusion_tag('blog/menu.html')
 def main_menu():
     menu = [
-        {'title': 'Articles', 'url_name': 'articles'},
-        {'title': 'Add Article', 'url_name': 'add_article'},
         {'title': 'Portfolio', 'url_name': 'portfolio'},
+        {'title': 'Contact', 'url_name': 'contact'},
         {'title': 'Sign In', 'url_name': 'login'}
     ]
 
--- privatesite/privatesite/settings.py
+++ privatesite/privatesite/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'privatesite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'portfoliodb',
+        'USER': 'chimmy',
+        'PASSWORD': 'QweQwe!23',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
