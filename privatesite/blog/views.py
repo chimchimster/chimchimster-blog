5de0e07a0d20ef0e661f11c02316d3e768d8e8e7
chimchimster <jorddyishere@gmail.com>
2023-01-27 12:21:33 +0500

    removed CBV

--- privatesite/blog/views.py
+++ privatesite/blog/views.py
@@ -1,13 +1,17 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 from .models import Portfolio
+
 
 def index(request):
     return render(request, 'blog/index.html')
 
+
 def get_portfolio(request, portfolio_slug):
-    portfolio = Portfolio.objects.get(pk=1)
+    portfolio = Portfolio.objects.filter(slug=portfolio_slug)
+    print(portfolio)
     return render(request, 'blog/index.html', {
         'portfolio': portfolio,
     })
